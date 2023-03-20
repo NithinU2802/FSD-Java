978d44c2605f0410001f011ebbd1fbec9e381493
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2023-03-20 18:09:59 +0530

    Create PrintStatement.java

---
+++ Java Core/PrintStatement.java
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Possible value and techniques to print values...
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	// println(arg);
+	System.out.println("Name"+9);
+	System.out.println(9+"Name");
+	System.out.println('a'+10);
+	System.out.println('a'+"Name");
+	System.out.println('a'+"Name");
+	System.out.println('a'+"Name"+10*20);
+	System.out.println('a'+"Name"+20/10);
+	System.out.println('a'+"Name"+20%10);
+	System.out.println('a'+"Name");
+	
+	//print(arg);
+	System.out.print("Name"+90+"\n");
+	
+	//printf(arg1,arg2);
+	System.out.printf("Name %d\n",89);
+	
+	// use of \n \b \t 
+	System.out.print("Name\n Name\b Name\t");
+	
+	}
+}
